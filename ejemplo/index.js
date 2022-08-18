ba6d89fd93ffbf4037f5cfb5dfe51d78198076cc
Tzuzul <mail@tzuzulcode.com>
2022-08-18 09:12:38 -0500

    Express y peticion

--- ejemplo/index.js
+++ ejemplo/index.js
@@ -1,13 +1,32 @@
 const express = require("express")
+const { port } = require("./config")
 
 const app = express()
-
-const port = 4000
 
 app.get("/",function(req,res){
     console.log(req)
 
-    return res.end("Hola mundo")
+    return res.end("GET")
+})
+
+app.get("/:id",function(req,res){
+
+    return res.end("GET:"+req.params.id)
+})
+
+app.post("/",function(req,res){
+
+    return res.end("POST")
+})
+
+app.put("/:id",function(req,res){
+
+    return res.end("PUT:"+req.params.id)
+})
+
+app.delete("/:id",function(req,res){
+
+    return res.end("DELETE:"+req.params.id)
 })
 
 app.listen(port,()=>{
