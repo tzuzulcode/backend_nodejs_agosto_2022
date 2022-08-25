06bd537d4e6b547fe93199aa8a23ccefbecba493
Tzuzul <mail@tzuzulcode.com>
2022-08-25 08:41:47 -0500

    Routes corrected

--- ejemplo/routes/food_simple.js
+++ ejemplo/routes/food_simple.js
@@ -11,26 +11,27 @@ function food(app) {
     const food = await foodServ.getAll();
     return res.status(food.success ? 200 : 400).json(food);
   });
+
+  router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = await foodServ.getById(id);
+    return res.status(food.success ? 200 : 400).json(data);
+  });
+
   router.post("/", async (req, res) => {
     const data = req.body;
     const food = await foodServ.create(data);
     return res.status(food.success ? 200 : 400).json(food);
   });
 
-  router.put("/food/:id", async (req, res) => {
+  router.put("/:id", async (req, res) => {
     const { id } = req.params;
     const body = req.body;
-    const data = await foodServ.edit(id, body);
+    const data = await foodServ.update(id, body);
     return res.status(food.success ? 200 : 400).json(data);
   });
 
-  router.get("/food/:id", async (req, res) => {
-    const { id } = req.params;
-    const data = await foodServ.getById(id);
-    return res.status(food.success ? 200 : 400).json(data);
-  });
-
-  router.delete("/food/:id", async (req, res) => {
+  router.delete("/:id", async (req, res) => {
     const { id } = req.params;
     const data = await foodServ.delete(id);
     return res.status(food.success ? 200 : 400).json(data);
