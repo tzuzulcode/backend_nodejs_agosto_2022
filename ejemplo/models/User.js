8742293c7b8685718ee1fe595eacd8a31092c269
Ulver <lorito.free@gmail.com>
2022-08-29 23:05:36 -0400

    Agregado el uso de token para la autenticación

---
+++ ejemplo/models/User.js
@@ -0,0 +1,16 @@
+const { mongoose } = require("../config/db")
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+})
+
+const UserModel = mongoose.model("User", userSchema)
+
+module.exports = UserModel
